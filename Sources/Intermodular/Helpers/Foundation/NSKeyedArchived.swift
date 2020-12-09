0529213efd34d03d28e2593bfa365d76bf5a576a
Vatsal Manot <vatsal.manot@yahoo.com>
2020-12-09 00:14:11 -0500

    Update package

--- Sources/Intermodular/Helpers/Foundation/NSKeyedArchived.swift
+++ Sources/Intermodular/Helpers/Foundation/NSKeyedArchived.swift
@@ -6,11 +6,16 @@ import Foundation
 import Swallow
 
 @propertyWrapper
-public struct NSKeyedArchived<Value: NSCoding>: Codable {
+public struct NSKeyedArchived<Value>: Codable {
     public var wrappedValue: Value
     
     @inlinable
-    public init(wrappedValue: Value) {
+    public init(wrappedValue: Value) where Value: NSCoding {
+        self.wrappedValue = wrappedValue
+    }
+    
+    @inlinable
+    public init<T>(wrappedValue: Value) where T: NSCoding, Value == Optional<T> {
         self.wrappedValue = wrappedValue
     }
     
@@ -18,45 +23,33 @@ public struct NSKeyedArchived<Value: NSCoding>: Codable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
         
-        self.init(wrappedValue: try Value.init(coder: try NSKeyedUnarchiver(forReadingFrom: try container.decode(Data.self))).unwrap())
+        if let type = Value.self as? NSCoding.Type {
+            wrappedValue = try type.init(coder: try NSKeyedUnarchiver(forReadingFrom: try container.decode(Data.self))).unwrap() as! Value
+        } else {
+            let type = (Value.self as! _opaque_Optional.Type)._opaque_Optional_Wrapped as! NSCoding.Type
+            
+            if container.decodeNil() {
+                wrappedValue = (Value.self as! _opaque_Optional.Type).init(nilLiteral: ()) as! Value
+            } else {
+                wrappedValue = try type.init(coder: try NSKeyedUnarchiver(forReadingFrom: try container.decode(Data.self))).unwrap() as! Value
+            }
+        }
     }
     
     @inlinable
     public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
         
-        try container.encode(try NSKeyedArchiver.archivedData(withRootObject: wrappedValue, requiringSecureCoding: wrappedValue is NSSecureCoding))
+        if let wrappedValue = wrappedValue as? _opaque_Optional, wrappedValue.isNil {
+            try container.encodeNil()
+        } else {
+            try container.encode(try NSKeyedArchiver.archivedData(withRootObject: wrappedValue as! NSCoding, requiringSecureCoding: wrappedValue is NSSecureCoding))
+        }
     }
 }
 
-@propertyWrapper
-public struct NSKeyedArchivedOptional<Value: NSCoding>: Codable {
-    public var wrappedValue: Value?
-    
-    @inlinable
-    public init(wrappedValue: Value?) {
-        self.wrappedValue = wrappedValue
-    }
-    
-    @inlinable
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        
-        if container.decodeNil() {
-            self.init(wrappedValue: nil)
-        } else {
-            self.init(wrappedValue: Value.init(coder: try NSKeyedUnarchiver(forReadingFrom: try container.decode(Data.self))))
-        }
-    }
-    
-    @inlinable
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        
-        if let wrappedValue = wrappedValue {
-            try container.encode(try NSKeyedArchiver.archivedData(withRootObject: wrappedValue, requiringSecureCoding: wrappedValue is NSSecureCoding))
-        } else {
-            try container.encodeNil()
-        }
+extension NSKeyedArchived where Value: ExpressibleByNilLiteral & NSCoding {
+    public init(nilLiteral: Void) {
+        self.init(wrappedValue: .init(nilLiteral: ()))
     }
 }
