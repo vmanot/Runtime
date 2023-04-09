9da3e47bf9dc3b1a06f50f0f0364d8603431f143
Vatsal Manot <vatsal.manot@yahoo.com>
2023-04-09 15:33:00 +0530

    Update package

--- Sources/Runtime/Intermodular/Helpers/Swift/OpaqueExistentialContainerInterface.swift
+++ Sources/Runtime/Intermodular/Helpers/Swift/OpaqueExistentialContainerInterface.swift
@@ -239,14 +239,3 @@ extension TypeMetadata {
 private func _strictlyUnoptimized_passOpaqueExistential(_ value: Any) -> Any {
     return value
 }
-
-public func _isValueOfGivenType<Value>(
-    _ value: Value,
-    type: Any.Type
-) -> Bool {
-    func check<T>(_ type: T.Type) -> Bool {
-        value is T
-    }
-    
-    return _openExistential(type, do: check)
-}
